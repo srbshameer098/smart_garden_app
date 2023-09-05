2a5e69646f88d568e6a20ead1dff252253117cd9
srbshameer098 <srbshameer098@gmail.com>
2023-09-05 15:43:36 +0530

    four

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+
 
   @override
   State<Home> createState() => _HomeState();
 }
 
+
+
+
 class _HomeState extends State<Home> {
+  final _controller = PageController();
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
+      backgroundColor: Color(0xffEBFCF8),
       body: Column(
         children: [
           Padding(
-            padding:  EdgeInsets.only(left:24.w ,top: 50.h,right: 24.w),
+            padding:  EdgeInsets.only(left:24.w ,top: 60.h,right: 24.w),
             child: Row(
               children: [
 
@@ -28,7 +38,7 @@ class _HomeState extends State<Home> {
                 ),
 SizedBox(width: 84.w,),
 
-                Icon(Icons.settings,color: Color(0xff0C9359),)
+                Icon(Icons.settings,color: Color(0xff0C9359),),
 
               ],
             ),
@@ -36,13 +46,138 @@ SizedBox(width: 84.w,),
 SizedBox(height: 20.h,),
           Padding(
             padding:  EdgeInsets.only(left: 15.w),
-            child: Row(
-              children: [
-                Image.asset("assets/fsp1.png",
-                  width: 266,
-                  height: 116,
+            child: SizedBox(height: 190,
+              child: PageView(
+                controller: _controller,
+                children: [
+                  Center(
+                    child: Image.asset("assets/fsp1.png",
+                      width: 333.w,
+                      height: 186.h,
+                    ),
+                  ),
+                  Center(
+                    child: Image.asset("assets/fsp2.png",
+                      width: 333.w,
+                      height: 186.h,
+                    ),
+                  ),
+                  Center(
+                    child: Image.asset("assets/fsp3.png",
+                      width: 333.w,
+                      height: 186.h,
+                    ),
+                  ),
 
+
+                ],
+              ),
+
+            ),
+
+          ),
+
+          SizedBox(height: 20.h),
+          SmoothPageIndicator(
+            controller: _controller,
+            count: 3,
+            axisDirection: Axis.horizontal,
+            effect: const SlideEffect(
+              activeDotColor: Colors.white54,
+              dotHeight: 8,
+              dotColor: Color(0xff0C9359),
+              dotWidth: 16,
+            ),
+          ),
+          SizedBox(height: 20.h),
+
+
+          Padding(
+            padding:  EdgeInsets.only(left: 26.w,top: 32.h,right: 26.w),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Container(
+                        width: 94,
+                        height: 94,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            color: Colors.white),
+                      child: Column(
+                        children: [
+                          Icon(Icons.humi)
+                        ],
+                      ),
+                    ),
+SizedBox(width: 8.w,),
+                    Container(
+                        width: 94,
+                        height: 94,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            color: Colors.white)
+                    ),
+SizedBox(width: 8.w,),
+                    Container(
+                        width: 94,
+                        height: 94,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            color: Colors.white)
+                    )
+
+
+                  ],
                 ),
+
+                SizedBox(height: 8.h,),
+                Row(
+                  children: [
+
+                    Container(
+                        width: 94,
+                        height: 94,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            color: Colors.white)
+                    ),
+                    SizedBox(width: 8.w,),
+                    Container(
+                        width: 196,
+                        height: 94,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            color: Colors.white)
+                    ),
+                  ],
+                ),
+                SizedBox(height: 8.h,),
+
+                Row(
+                  children: [
+
+                    Container(
+                        width: 196,
+                        height: 94,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            color: Colors.white)
+                    ),
+                    SizedBox(width: 8.w,),
+
+                    Container(
+                        width: 94,
+                        height: 94,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            color: Colors.white)
+                    ),
+
+                  ],
+                ),
+
+
               ],
             ),
           )
@@ -51,8 +186,23 @@ SizedBox(height: 20.h,),
 
 
 
+
         ],
       ),
     );
+  }
+}
+
+
+
+
+
+class Modul1 extends StatelessWidget {
+  final _controller = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
   }
 }
